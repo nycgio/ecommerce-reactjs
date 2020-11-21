e6111bf411f83a2bb00ef0d95dcc35de26a1390c
nycgio <giorgialvarez@gmail.com>
2020-11-21 15:23:00 -0500

    Created react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+// Screens
+import Home from './screens/Home'
+import ProductScreen from './screens/ProductScreen'
+
 function App() {
   return (
-    <>
-      <h1>Ecommerce ReactJS</h1>
-    </>
-  );
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={Home} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
