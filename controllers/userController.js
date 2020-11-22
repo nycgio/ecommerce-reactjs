c8b17b31eae541f8702d36d629faab0d1c6d7fd3
nycgio <giorgialvarez@gmail.com>
2020-11-22 10:36:52 -0500

    created register user and login methods server side

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,4 +45,38 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-module.exports = { authUser, getUserProfile }
+// @desc    Register a user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  //   find if user exist
+  const userExist = await User.findOne({ email })
+
+  if (userExist) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
+
+module.exports = { authUser, getUserProfile, registerUser }
--- models/User.js
+++ models/User.js
@@ -30,6 +30,17 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcryptjs.compare(enteredPassword, this.password)
 }
 
+// before user model save encrypt the password
+userSchema.pre('save', async function (next) {
+  // only do this when the password is actually changed
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcryptjs.genSalt(10)
+  this.password = await bcryptjs.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = User
