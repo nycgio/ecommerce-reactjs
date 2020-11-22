054183c0754b880e6c55cbf2cf3b31812c6301b1
nycgio <giorgialvarez@gmail.com>
2020-11-22 07:38:53 -0500

    fixed a bug where product doesn't clear

--- client/src/reducers/productReducers.js
+++ client/src/reducers/productReducers.js
@@ -28,7 +28,7 @@ export const productDetailsReducer = (
 ) => {
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
-      return { loading: true, ...state }
+      return { loading: true, ...state, product: [] }
 
     case PRODUCT_DETAILS_SUCCESS:
       return { loading: false, product: action.payload }
