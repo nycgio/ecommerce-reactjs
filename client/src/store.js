6227fc3472bee44bf60ede5eacd7b956ca29ad1f
nycgio <giorgialvarez@gmail.com>
2020-11-28 14:29:38 -0500

    Created a order screen

--- client/src/store.js
+++ client/src/store.js
@@ -15,7 +15,10 @@ import {
   userRegisterReducer,
   userUpdateProfileReducer,
 } from './reducers/userReducers'
-import { orderCreateReducer } from './reducers/orderReducers'
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+} from './reducers/orderReducers'
 
 // combine all our reducers
 const reducer = combineReducers({
@@ -27,6 +30,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
 })
 
 // get local storage
