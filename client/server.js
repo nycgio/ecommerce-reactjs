b7576e3442c44db7d37210a9d46559079d57a642
nycgio <giorgialvarez@gmail.com>
2020-11-28 12:28:43 -0500

    Added a rate limiter on the http server

--- client/server.js
+++ client/server.js
@@ -1,10 +1,31 @@
 const express = require('express')
-const http = require('http')
-const https = require('https')
+const limiter = require('express-rate-limit')
 
 const app = express()
 const port = 80
 
+// add limit to requests
+const time = 1
+
+const limit = limiter({
+  windowMs: time * 60 * 1000,
+  max: 180,
+  message: `Too many requests from ip please try again in ${time} ${
+    time > 1 ? 'minutes' : 'minute'
+  }`,
+  handler: function (req, res, next) {
+    let ip = req.headers['x-real-ip'] || req.connection.remoteAddress
+    if (ip.substr(0, 7) === '::ffff:') {
+      ip = ip.substr(7)
+    }
+    let customMessage = `Too many requests from ${ip} please try again in ${time} ${
+      time > 1 ? 'minutes' : 'minute'
+    }`
+    res.send(customMessage)
+  },
+})
+
+app.use(limit)
 app.use(express.static('build'))
 
 app.listen(port, () => console.log('http server running'))
